e0ac583503c1e2dfc1b661020110fda365009350
Jean Claveau <1556489+jclaveau@users.noreply.github.com>
2026-04-19 06:27:00 +0200

    chore: revert system-data change

--- packages/system-data/src/fields/index.ts
+++ packages/system-data/src/fields/index.ts
@@ -1,13 +1,10 @@
-import type { FieldIndex, FieldMeta } from '../types.js';
 import defaults from './_defaults.yaml';
+
 import accessFields from './access.yaml';
 import activityFields from './activity.yaml';
 import collectionFields from './collections.yaml';
 import commentsFields from './comments.yaml';
 import dashboardFields from './dashboards.yaml';
-import deploymentProjectsFields from './deployment-projects.yaml';
-import deploymentRunsFields from './deployment-runs.yaml';
-import deploymentFields from './deployment.yaml';
 import extensionFields from './extensions.yaml';
 import fieldFields from './fields.yaml';
 import fileFields from './files.yaml';
@@ -29,19 +26,11 @@ import shareFields from './shares.yaml';
 import translationFields from './translations.yaml';
 import userFields from './users.yaml';
 import versionFields from './versions.yaml';
+import webhookFields from './webhooks.yaml';
+
+import type { FieldMeta } from '../types.js';
 
 export const systemFieldRows: FieldMeta[] = [];
-export const systemIndexRows: FieldIndex[] = [];
-
-export function hasSystemIndex(collection: string, field: string): boolean {
-	return Boolean(systemIndexRows.find((row) => row.collection === collection && row.field === field));
-}
-
-export function isSystemField(collection: string, field: string): boolean {
-	if (!collection.startsWith('directus_')) return false;
-
-	return Boolean(systemFieldRows.find((fieldMeta) => fieldMeta.collection === collection && fieldMeta.field === field));
-}
 
 processFields(accessFields);
 processFields(activityFields);
@@ -69,33 +58,18 @@ processFields(shareFields);
 processFields(translationFields);
 processFields(userFields);
 processFields(versionFields);
-processFields(deploymentFields);
-processFields(deploymentProjectsFields);
-processFields(deploymentRunsFields);
+processFields(webhookFields);
 
 function processFields(systemFields: Record<string, any>) {
-	const {
-		table: collection,
-		fields,
-		indexed,
-	} = systemFields as { table: string; fields: FieldMeta[]; indexed: Pick<FieldIndex, 'field'>[] | undefined };
+	const { fields, table } = systemFields as { fields: FieldMeta[]; table: string };
 
 	fields.forEach((field, index) => {
 		systemFieldRows.push({
 			system: true,
 			...defaults,
 			...field,
-			collection,
+			collection: table,
 			sort: index + 1,
 		});
 	});
-
-	if (indexed) {
-		indexed.forEach(({ field }) => {
-			systemIndexRows.push({
-				collection,
-				field,
-			});
-		});
-	}
 }
