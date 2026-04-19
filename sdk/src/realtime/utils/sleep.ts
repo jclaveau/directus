efb1dd04646820a124092026cd957ef4603c778d
Jean Claveau <1556489+jclaveau@users.noreply.github.com>
2026-04-19 09:01:58 +0200

    style: format

--- sdk/src/realtime/utils/sleep.ts
+++ sdk/src/realtime/utils/sleep.ts
@@ -3,4 +3,5 @@
  * @param delay number in MS
  * @returns void
  */
-export const sleep = (delay: number): Promise<void> => new Promise<void>((resolve) => setTimeout(() => resolve(), delay));
+export const sleep = (delay: number): Promise<void> =>
+	new Promise<void>((resolve) => setTimeout(() => resolve(), delay));
